d0c7c41fdddb7a149c65c3e382595cf262e127d1
Sebastian Spannekrebs <sebastian@spannekrebs.com>
2022-09-02 10:14:00 +0200

    added files to repo

--- src/main/java/com/example/itsystemhausddgmbh/Symbolcontroller.java
+++ src/main/java/com/example/itsystemhausddgmbh/Symbolcontroller.java
@@ -5,18 +5,22 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Button;
 
 public class Symbolcontroller {
-    @FXML
-    private Label welcomeText;
-    @FXML
-    private Button button1;
-    private Button button2;
-    private Button button3;
-    private Button button4;
-    private Button button5;
+    @FXML private Label welcomeText;
+    @FXML private Button button1;
+    @FXML private Button button2;
+    @FXML private Button button3;
+    @FXML private Button button4;
+    @FXML private Button button5;
+
+    public Symbolcontroller() {
+    }
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("IT SystemHaus GmbH v1.0.0");
+    }
+    protected void onButtonOneCLick() {
+
     }
 
 }
