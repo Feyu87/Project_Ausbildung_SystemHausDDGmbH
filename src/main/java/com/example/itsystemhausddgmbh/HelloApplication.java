ad56e62e06be74c15072aeb61123464e42d7a42e
Sebastian Spannekrebs <sebastian@spannekrebs.com>
2022-09-04 14:05:21 +0200

    - added files - replaced fxml - set platform exit function

--- src/main/java/com/example/itsystemhausddgmbh/HelloApplication.java
+++ src/main/java/com/example/itsystemhausddgmbh/HelloApplication.java
@@ -10,9 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("itsystemhausddgmbh.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("IT Systemhaus GmbH");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("itsystemhaus.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        stage.setTitle("IT Systemhaus Dresden GmbH");
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/itsystemhausddgmbh/Symbolcontroller.java
+++ src/main/java/com/example/itsystemhausddgmbh/Symbolcontroller.java
@@ -1,26 +1,29 @@
 package com.example.itsystemhausddgmbh;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
 
 public class Symbolcontroller {
-    @FXML private Label welcomeText;
     @FXML private Button button1;
     @FXML private Button button2;
     @FXML private Button button3;
     @FXML private Button button4;
     @FXML private Button button5;
+    @FXML private Button button6;
+
 
     public Symbolcontroller() {
-    }
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("IT SystemHaus GmbH v1.0.0");
-    }
-    protected void onButtonOneCLick() {
 
     }
+    @FXML protected void closeProgramm() {
+        Platform.exit();
+    }
+
+
+
+
 
 }
